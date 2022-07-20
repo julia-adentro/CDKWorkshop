3c5f270d53e491b877870e544456f2526285a5a3
julia-adentro <jkarkowska@adentro.com>
2022-07-20 08:36:48 -0400

    Creating a bucket

--- lib/cdk_workshop-stack.ts
+++ lib/cdk_workshop-stack.ts
@@ -1,16 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib'
 
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }

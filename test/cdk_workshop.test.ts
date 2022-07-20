753feccca7afb0a346558bf0197cb89b61b7ed1d
julia-adentro <jkarkowska@adentro.com>
2022-07-20 07:49:20 -0400

    cdk init app

---
+++ test/cdk_workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWorkshop from '../lib/cdk_workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
